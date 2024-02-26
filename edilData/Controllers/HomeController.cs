b5004d3da7cada07d22e2ee4e40595347f4edd9a
RiccardoAntonini95 <riccardo.antonini95@gmail.com>
2024-02-26 16:43:37 +0100

    Aggiungere i file di progetto.

---
+++ edilData/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using edilData.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace edilData.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult EmployeeForm()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ edilData/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace edilData.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
